b5a45d5257d3cabd3c5e1625bc5c5be303214013
ramyashrict <ramyarr1415@gmail.com>
2023-04-05 19:01:22 +0530

    stack push pop queue de queue added

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StackQueueLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueueLinkedList
+{
+     class Stack
+    {
+             public static void Main(string[] args)
+            {
+            Stack st = new Stack();
+                st.Push(70);
+                st.Push(30);
+                st.Push(56);
+
+                foreach (Object obj in st)
+                {
+                    Console.WriteLine(obj);
+                }
+                Console.WriteLine(); Console.WriteLine();
+                Console.WriteLine("The number of elements in the stack " + st.Count);
+                Console.WriteLine("Does the stack contain the elements 3 " + st.Contains(3));
+                Console.ReadKey();
+            }
+        }
+    }
+
+
